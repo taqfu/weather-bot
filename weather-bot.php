5024a3c36e6f3a2330eb983ea2846f348aa89134
taqfu <taqfu0@gmail.com>
2017-07-31 16:07:00 -0700

    AccuWeather blocked me. Switched to wttr.in

--- weather-bot.php
+++ weather-bot.php
@@ -1,24 +1,24 @@
 <?php
-if (!file_exists("weather-bot-cfg.php")){
+if (!file_exists(getcwd() . "/weather-bot/weather-bot-cfg.php")){
     exit("Copy weather-bot-cfg.example.php to weather-bot-cfg.php and fill it out please.\n");
 }
-include("weather-bot-cfg.php");
 
+require("weather-bot-cfg.php");
 if (ACCUWEATHER_URL==NULL || MAX_HIGH_TEMP==NULL || MAX_LOW_TEMP==NULL){
     exit("Please fill out your weather-bot-cfg.php first\n");
 }
 $highest_temp_today = false;
+$already_messaged=false;
 $ch = curl_init(ACCUWEATHER_URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $weather_report = curl_exec($ch);
 curl_close($ch);
-//$weather_report = curl-this-shit
-$temp_now =  strstr(substr(strstr($weather_report, "local-temp"), 12, 20), "&", true);
-echo "Temperature now is:" . $temp_now . "\n";
-
+$report_arr = explode("\n", $weather_report);
+$span_arr = explode("</span>", $report_arr[301]);
+$rel_str = substr($span_arr[1], -3);
+$temp_now = substr($rel_str, 0, 1) == ">" ?  substr($rel_str, 1) : $rel_str;
 $date = date("m-d-Y");
-$filename = "history/" . $date . ".json";
-echo "Searching for " . $filename . "\n";
+$filename = getcwd() . "/weather-bot/history/" . $date . ".json";
 if (!file_exists($filename)){
     update_history($filename, [time()=>$temp_now]);
     $highest_temp_today=true;
@@ -27,6 +27,9 @@ if (!file_exists($filename)){
 
     foreach ($temp_history as $time=>$temp){
         echo date("H:i:s", $time) . " - " . $temp . "\n";
+	if ($temp>=MAX_HIGH_TEMP){
+		$already_messaged=true;
+	}
         if ($temp_now>$temp){
             $highest_temp_today = true;
         }
@@ -38,10 +41,11 @@ if (!file_exists($filename)){
     }
 }
 
-if ($temp_now>=MAX_HIGH_TEMP && $highest_temp_today){
-    echo "ALERT";
+if ($temp_now>=MAX_HIGH_TEMP && $highest_temp_today && !$already_messaged){
+
+	send_alert("It is now " . $temp_now . " degrees.");
 }
-function send_alert ($user, $msg){
+function send_alert ( $msg){
   curl_setopt_array($ch = curl_init(), array(
   CURLOPT_URL => "https://api.pushover.net/1/messages.json",
   CURLOPT_POSTFIELDS => array(
